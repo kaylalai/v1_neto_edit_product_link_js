41a63b40532bd2da34d7b29ec1ae1857e2f42cb3
Ka Yu Lai <61482931+kaylalai@users.noreply.github.com>
2021-06-16 13:16:08 +1000

    Update v1_neto_edit_product_link_script.js
    (1)make changed on the "onclick="window.open" for edit button to open the web in new tab

--- v1_neto_edit_product_link_script.js
+++ v1_neto_edit_product_link_script.js
@@ -1,11 +1,9 @@
-function addeditbutton() {
-
-    var productImages = [];//create empty array
+    var productImages = []; //create empty array
     var thumbs = document.querySelectorAll("img[src*='/assets/thumb']"); //thumb, thumbM, thumbL, thumbsdfsdfsfdfs will go in here
-    var fulls = document.querySelectorAll("img[src*='/assets/full']");//cant work with/asset
+    var fulls = document.querySelectorAll("img[src*='/assets/full']");
 
     for (var p = 0; p < thumbs.length; p++) //loop and add the result above to array,put two result in one array
-     {
+    {
         productImages.push(thumbs[p]);
     }
     for (var i = 0; i < fulls.length; i++) {
@@ -13,15 +11,16 @@ function addeditbutton() {
 
     }
     var skus = [];
-    for (var i = 0; i < productImages.length; i++) {//loop the new array ,it is the url
+    for (var i = 0; i < productImages.length; i++) { //loop the new array ,it is the url
         var imageurl = productImages[i].src; //take the src only
-        var sku = imageurl.substring(imageurl.lastIndexOf('/') + 1, imageurl.lastIndexOf('.'));//only take some part
+        var sku = imageurl.substring(imageurl.lastIndexOf('/') + 1, imageurl.lastIndexOf('.')); //only take some part
         skus.push(sku); //use push = add the result sku to emty array
-        productImages[i].parentElement.outerHTML += '<button class="edit-button" style="border:1px solid #c0c0c0;background-color:#6a0dad;color:#fff;position:absolute;top:0;left:0;margin:5px;padding:2px" onclick="location.href=\'/_cpanel/products/view?sku='+skus[i]+'\'">edit</button>';
-//put each sku in the button href
- //assign button, not work for innerHTML,is outside cannt not link the button to sku
-//not work for href 
-//sytan have to becareful in location.href
+        productImages[i].parentElement.outerHTML += '<button class="edit-button" style="border:1px solid #c0c0c0;position:absolute;top:0;left:0;margin:5px;padding:2px" onclick="window.open(\'/_cpanel/products/view?sku=' + skus[i] + '\')">edit</button>';
+
+        //put each sku in the button href
+        //assign button, not work for innerHTML,is outside cannt not link the button to sku
+        //not work for href
+        //sytan have to becareful in location.href
     }
 
 }
