d928b58331298168022363711196bf089dca666e
Ka Yu Lai <61482931+kaylalai@users.noreply.github.com>
2021-06-17 13:26:48 +1000

    Update v1_neto_etcontent_add_edit_button.js
    fix a spelling mistake

--- v1_neto_etcontent_add_edit_button.js
+++ v1_neto_etcontent_add_edit_button.js
@@ -14,7 +14,7 @@ function addmanagebutton() {
         var zoneid = contentzones[p].getAttribute("data-etcontentzone"); //show the value of attribute data in the data-etcontentzone
         console.log(zoneid)
         zone_id_array.push(zoneid);
-        contentzones[p].closeset("span").outerHTML += '<button class="edit-button" style="display: inline-block;background: #FF0000;color: rgba(255, 255, 255, 255);font-family:Trebuchet MS;font-size:8px;font-weight:bold;padding:2px 4px;text decoration:none;width: 20px;height: 20px;border-radius: 50%;text-align: center;border: solid 2px rgba(255, 255, 255, 0.47);" onclick="window.open(\'/_cpanel/contentzones/view?id=' + zone_id_array[p] + '\')">E</button>';
+        contentzones[p].closest("span").outerHTML += '<button class="edit-button" style="display: inline-block;background: #FF0000;color: rgba(255, 255, 255, 255);font-family:Trebuchet MS;font-size:8px;font-weight:bold;padding:2px 4px;text decoration:none;width: 20px;height: 20px;border-radius: 50%;text-align: center;border: solid 2px rgba(255, 255, 255, 0.47);" onclick="window.open(\'/_cpanel/contentzones/view?id=' + zone_id_array[p] + '\')">E</button>';
 
     }
 }
