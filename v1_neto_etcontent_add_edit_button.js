d251c0942cbf17f6bf29118c3017db04a6ffeb5e
Ka Yu Lai <61482931+kaylalai@users.noreply.github.com>
2021-06-20 22:40:04 +1000

    Update v1_neto_etcontent_add_edit_button.js
    make change with the button position

--- v1_neto_etcontent_add_edit_button.js
+++ v1_neto_etcontent_add_edit_button.js
@@ -15,7 +15,7 @@ function addmanagebutton() {
         console.log(zoneid)
         zone_id_array.push(zoneid);
 
-        contentzones[p].innerHTML += '<button class="edit-button" style="display: inline-block;background: #FF0000;color: rgba(255, 255, 255, 255);font-family:Trebuchet MS;font-size:8px;font-weight:bold;padding:2px 4px;width: 20px;height: 20px;border-radius: 50%;text-align: center;border: solid 2px rgba(255, 255, 255, 0.47);z-index:1;position: absolute;" onclick="window.open(\'/_cpanel/contentzones/view?id=' + zone_id_array[p] + '\')">E</button>';
+        contentzones[p].innerHTML += '<button class="edit-button" style="background: #FF0000;color: rgba(255, 255, 255, 255);font-family:Trebuchet MS;font-size:8px;font-weight:bold;padding:2px 4px;width: 20px;height: 20px;border-radius: 50%;text-align: center;border: solid 2px rgba(255, 255, 255, 0.47);z-index:1;" onclick="window.open(\'/_cpanel/contentzones/view?id=' + zone_id_array[p] + '\')">E</button>';
 
     }
 }
